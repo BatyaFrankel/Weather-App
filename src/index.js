7a2bf5afdccb0ae89d209f9a9fef9e15435a79a7
BatyaFrankel <frankelbatya@gmail.com>
2024-12-05 20:24:41 +0000

    injected more weather data, time and weather icon

--- src/index.js
+++ src/index.js
@@ -16,16 +16,48 @@ function cityInput(event) {
 
 function displayRefreash(responce) {
   console.log(responce);
-  let currentTemperature = Math.round(responce.data.temperature.current);
-  let temperatureDisplay = document.querySelector("#current-temperature");
-  temperatureDisplay.innerHTML = currentTemperature;
-
-  let currentCity = responce.data.city;
   let cityDisplay = document.querySelector("#current-city");
+  let currentCity = responce.data.city;
+  let currentTemperature = Math.round(responce.data.temperature.current);
+  let temperatureElement = document.querySelector("#current-temperature");
+  let iconElement = document.querySelector("#temperature-icon");
+  let dateElement = document.querySelector("#time");
+  let conditionElement = document.querySelector("#condition");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+
   cityDisplay.innerHTML = currentCity;
+  temperatureElement.innerHTML = currentTemperature;
+  iconElement.innerHTML = `<img src="${responce.data.condition.icon_url}" class="weather-app-icon" />`;
+  conditionElement.innerHTML = responce.data.condition.description;
+  humidityElement.innerHTML = responce.data.temperature.humidity;
+  windElement.innerHTML = `${responce.data.wind.speed}km/h`;
+
+  let currentDate = new Date(responce.data.time * 1000);
+
+  dateElement.innerHTML = dateFormat(currentDate);
+  console.log(currentDate);
+}
+function dateFormat(date) {
+  let now = date;
+  let hour = now.getHours().toString().padStart(2, "0");
+  let min = now.getMinutes().toString().padStart(2, "0");
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  return `${day} ${hour}:${min}`;
 }
 
 let formInput = document.querySelector("#input-city-form");
 formInput.addEventListener("submit", cityInput);
 
 search("London");
+//
